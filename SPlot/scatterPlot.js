0a701e4131cedfdeec273e61a2cc4a4b94f17f0a
gabe3 <Dinya7991>
2018-11-19 00:17:23 -0500

    Task: Started to Plot the Circle but failed, Made a TotalData object that holds both chloride and density arrays together.

--- SPlot/scatterPlot.js
+++ SPlot/scatterPlot.js
@@ -2,6 +2,8 @@
 var width = 1000;
 var height = 500;
 
+
+
 //Import the data set and isolate the density and chlorides
 d3.csv("WhiteWineCSV.csv", (data) => {
 
@@ -15,6 +17,12 @@ d3.csv("WhiteWineCSV.csv", (data) => {
     let density = data.map((y) => {
         return parseFloat(y.density)
     });
+
+
+    //Make A Total Data Object that hold both of the x and y arrays.
+    let totalData = {x: chlorides,
+                     y: density};
+    console.log(totalData.x);
 
 
     // Create the X and Y Axis
@@ -36,20 +44,37 @@ d3.csv("WhiteWineCSV.csv", (data) => {
     let xAxisTranslate = height/2 + 10;
     svg.append("g").attr("transform","translate(60, "+ xAxisTranslate + " )").call(x_Axis);
 
+    //Map Data
+    //let xMap = function(){return xScale(chlorides[])};
+    //let yMap = ()=> {return yScale(density[60])};
+
 
     //Add Text to the Side
 
-    //Y Axis
+    //Y Axis Text
     svg.append("text")
         .style("font-size","20px")
         .attr("text-anchor","middle")
         .attr("transform", "translate(20,120), rotate(-90)")
         .text("Density");
 
+    //X Axis Text
     svg.append("text")
         .style("font-size","20px")
         .attr("text-anchor","middle")
         .attr("transform", "translate(500,300)")
         .text("Chloride");
+
+    //Plot the Circle
+    let circles = svg.selectAll('circle').data(data);
+
+        circles.enter()
+            .append('circle')
+            .attr("r", 1.5)
+            .attr("class", "dot")
+            //.attr("cx",)
+            //.attr("cy",)
+            //.merge(circles);
+
 
 });
